8d1431ec4fd8ce316cdfa4b358284c4ea0dc7cff
ZhekaiJin <jinzhekai0705@163.com>
2018-04-27 12:22:06 -0400

    complete project

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -18,7 +18,6 @@ import org.json.JSONObject;
 import db.DBConnection;
 import db.DBConnectionFactory;
 import entity.Item;
-import external.TicketMasterAPI;
 
 /**
  * Servlet implementation class SearchItem
